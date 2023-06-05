ff30799645873cb17cedb2d5821e391775d89164
Bo <bhsuhkr@naver.com>
2023-06-04 20:54:14 -0500

    git init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Transactions></Transactions>
+</template>
+
+<script>
+import Transactions from './components/Transactions.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Transactions
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Transactions.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello world!
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Transactions',
+}
+</script>
+
+<style scoped>
+    
+</style>
