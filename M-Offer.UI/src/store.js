948401bef69ca5c6446a1df18ba1c27d5b9f19ac
Bo VPN <bhsuhkr@naver.com>
2023-06-11 21:10:21 -0500

    todo

--- M-Offer.UI/src/store.js
+++ M-Offer.UI/src/store.js
@@ -23,12 +23,11 @@ export const useAuthStore = defineStore('auth', {
       await axios.post('http://localhost:3000/api/auth/signin', userData)
         .then(response => {
           console.log("login successfully", response);
-          // Handle the response from the backend
           this.setUser(cred.username);
+          // TODO: generate a user token
           this.setToken("test");
           localStorage.setItem("userToken", this.token);
           router.push('/pay');
-
         })
         .catch(error => {
           console.error("Axios Login failed", error);
