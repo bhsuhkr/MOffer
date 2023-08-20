40bbcc15400b59fdeb85d3b0b8e972fa366a4af3
Jung Yang <jung.y.yang@gmail.com>
2023-08-20 16:45:50 -0500

    reporting related queries

---
+++ DBScripts/report_quries.sql
@@ -0,0 +1,18 @@
+
+-- query to get all debit and credits for a given day
+-- you can change the string date value folloing mm-dd-yyyy format
+select nc_transactions.TransType, sum(nc_transactions.transamount)
+from nc_transactions 
+where CONVERT(DATE, TransTime) = CONVERT(DATE, '08-20-2023')
+group by nc_transactions.TransType
+order by nc_transactions.TransType desc
+
+
+-- query to get refunds
+-- this query finds all credit records with amount below 5 for a given day
+select nc_transactions.MemberID, nc_transactions.TransType, nc_transactions.TransTime, nc_transactions.TransPmntMethod, nc_transactions.RunningBalance,transamount, nc_members.KoreanName, nc_members.ContId
+from nc_transactions 
+left join nc_members on nc_transactions.memberid = nc_members.memberid
+where CONVERT(DATE, TransTime) = CONVERT(DATE, '8-20-2023')
+and transtype in ('CREDIT', 'REFUND', 'CREDIT_ADM', 'CREDIT_SYS') and TransAmount < 5
+order by TransTime desc
