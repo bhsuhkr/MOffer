94c2214ae0a606d5b48614da3513d49cd2634e12
Jung Yang <jung.y.yang@gmail.com>
2023-08-22 23:44:09 -0500

    query to get unique number of members who had transaction for a given day

--- DBScripts/report_quries.sql
+++ DBScripts/report_quries.sql
@@ -8,6 +8,12 @@ group by nc_transactions.TransType
 order by nc_transactions.TransType desc
 
 
+-- query to get unique number of members who had transaction for a given day
+-- you can change the string date value folloing mm-dd-yyyy format
+select count(distinct nc_transactions.MemberID)
+from nc_transactions 
+where CONVERT(DATE, TransTime) = CONVERT(DATE, '08-20-2023')
+
 -- query to get refunds
 -- this query finds all credit records with amount below 5 for a given day
 select nc_transactions.MemberID, nc_transactions.TransType, nc_transactions.TransTime, nc_transactions.TransPmntMethod, nc_transactions.RunningBalance,transamount, nc_members.KoreanName, nc_members.ContId
