185e7d848e47cb6e1f6d6a2b43c7da8046311a88
Bo VPN <bhsuhkr@naver.com>
2024-01-07 17:49:37 -0600

    validations

--- M-Offer.UI/src/components/Cafe.vue
+++ M-Offer.UI/src/components/Cafe.vue
@@ -14,8 +14,10 @@
         />
         <button class="submit-btn" @click="handleEnterKey">Submit</button>
       </div>
+      <p class="validation-msg">{{ validationMessage }}</p>
 
-      <h4>Total: ${{ total }}</h4>
+      <h4>Total: ${{ total }}.00</h4>
+      <button class="pay-btn" @click="pay">Pay</button>
       <table class="table table-bordered table-text">
         <thead>
           <tr>
@@ -46,6 +48,8 @@ export default defineComponent({
     const titles = ["Product", "Price ($)"];
     let items = ref([]);
     let total = ref(0);
+    let barcodeField = ref("");
+    let validationMessage = ref("");
 
     const deleteRow = (index) => {
       const deletedPrice = parseFloat(items.value[index].price.replace("$", ""));
@@ -53,7 +57,6 @@ export default defineComponent({
       total.value -= deletedPrice;
     };
 
-    let barcodeField = ref("");
     onMounted(() => {
       barcodeField.value.focus();
     });
@@ -62,8 +65,9 @@ export default defineComponent({
       titles,
       items,
       total,
-      deleteRow,
       barcodeField,
+      validationMessage,
+      deleteRow,
     };
   },
   methods: {
@@ -71,8 +75,13 @@ export default defineComponent({
       const barcode = this.$refs["barcodeField"].value;
       if (barcode) {
         const itemAndPrice = this.barcodeField.value.split("$");
-        this.total += parseFloat(itemAndPrice[1]);
-        this.items.push({ product: itemAndPrice[0], price: "$" + itemAndPrice[1] });
+        if (itemAndPrice[0] && itemAndPrice[1]) {
+          this.total += parseFloat(itemAndPrice[1]);
+          this.items.unshift({ product: itemAndPrice[0], price: "$" + itemAndPrice[1] });
+          this.validationMessage = "";
+        } else {
+          this.validationMessage = "잘못된 바코드입니다. 다시 시도해 주세요.";
+        }
       }
       this.$refs["barcodeField"].value = "";
       this.$refs.barcodeField.focus();
@@ -82,6 +91,9 @@ export default defineComponent({
       if (inputField != null) {
         inputField.focus();
       }
+    },
+    async pay() {
+      console.log("pay");
     },
   },
 });
@@ -94,6 +106,7 @@ export default defineComponent({
 }
 .input-row {
   display: flex;
+  margin-bottom: 20px;
 }
 .barcode-input {
   width: 100%;
@@ -107,5 +120,11 @@ export default defineComponent({
   margin-left: 10px;
   padding: 0 20px;
   max-width: 100px;
+}
+.validation-msg {
+  color: red;
+}
+.pay-btn {
+  margin-bottom: 10px;
 }
 </style>
