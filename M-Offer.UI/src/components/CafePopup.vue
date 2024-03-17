79d7110ae9a68382c1ed329ba26f23d1422da42d
Bo VPN <bhsuhkr@naver.com>
2024-03-17 16:28:56 -0500

    add order number

--- M-Offer.UI/src/components/CafePopup.vue
+++ M-Offer.UI/src/components/CafePopup.vue
@@ -3,14 +3,19 @@
     <div class="cafe-popup-content">
       <h3 id="cafePopupTotal" class="cafe-popup-total">Total: ${{ total.toFixed(2) }}</h3>
 
-      <div v-if="paymentType === 'BARCODE'">
+      <div v-if="barcodeType === 'ORDER_NUMBER'">
+        <h4 class="cafe-popup-header">주문 번호를 스캔해주세요.</h4>
+        <button @click="closePopup">취소</button>
+      </div>
+
+      <div v-else-if="barcodeType === 'USER' && paymentType === 'BARCODE'">
         <h4 class="cafe-popup-header">
-          {{ !isPaid ? "지불에 사용할 바코드를 스캔해 주세요." : "결제완료 되었습니다." }}
+          {{ !isPaid ? "계산에 사용할 바코드를 스캔해 주세요." : "결제완료 되었습니다." }}
         </h4>
         <button @click="closePopup">{{ isPaid ? "확인" : "취소" }}</button>
       </div>
 
-      <div v-else>
+      <div v-else-if="barcodeType === 'DISABLED' && (paymentType === 'CASH' || paymentType === 'CC')">
         <h4 class="cafe-popup-header">{{ !isPaid ? "결제가 완료되었습니까?" : "시스템에 기록되었습니다." }}</h4>
         <div class="cafe-popup-button-container" v-if="!isPaid">
           <button id="makePayment" @click="makePayment">확인</button>
@@ -18,6 +23,22 @@
         </div>
         <button id="paidConfirmation" @click="closePopup" v-else>확인</button>
       </div>
+
+      <!-- <div v-if="paymentType === 'BARCODE'">
+        <h4 class="cafe-popup-header">
+          {{ !isPaid ? "지불에 사용할 바코드를 스캔해 주세요." : "결제완료 되었습니다." }}
+        </h4>
+        <button @click="closePopup">{{ isPaid ? "확인" : "취소" }}</button>
+      </div> -->
+
+      <!-- <div v-else>
+        <h4 class="cafe-popup-header">{{ !isPaid ? "결제가 완료되었습니까?" : "시스템에 기록되었습니다." }}</h4>
+        <div class="cafe-popup-button-container" v-if="!isPaid">
+          <button id="makePayment" @click="makePayment">확인</button>
+          <button id="closePopup" @click="closePopup">취소</button>
+        </div>
+        <button id="paidConfirmation" @click="closePopup" v-else>확인</button>
+      </div> -->
     </div>
   </div>
 </template>
@@ -34,6 +55,10 @@ export default {
       required: true,
     },
     paymentType: {
+      type: String,
+      required: true,
+    },
+    barcodeType: {
       type: String,
       required: true,
     },
